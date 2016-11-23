6bf668246ed067624f2b2db84b3509f7915c405c
tlcong <tlcong@linagora.com>
2016-11-23 15:31:01 +0700

    Expose port for dev mode

--- develop.sh
+++ develop.sh
@@ -2,4 +2,4 @@
 echo "Build flow-proxy image"
 docker build -t flow-proxy .
 echo "Run flow-proxy server"
-docker run -it -v $(pwd):/usr/src/app --entrypoint ./entrypoint-dev.sh flow-proxy
+docker run -it -v $(pwd):/usr/src/app --entrypoint ./entrypoint-dev.sh -p 8080:8080  flow-proxy
