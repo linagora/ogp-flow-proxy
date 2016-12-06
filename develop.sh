2cdb7eca5f94cbc5f3becd3709d26b6cdce8f2a7
tlcong <tlcong@linagora.com>
2016-12-06 23:38:19 +0700

    Delete develop run file that is not nessary

--- develop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Build flow-proxy image"
-docker build -t flow-proxy .
-echo "Run flow-proxy server"
-docker run -it -v $(pwd):/usr/src/app --entrypoint ./entrypoint-dev.sh -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock:ro flow-proxy
